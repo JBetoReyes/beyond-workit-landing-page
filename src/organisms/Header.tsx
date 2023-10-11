570ee8d301db39dd9f707de8f3b2995e30de0426
Beto <josereyes55@hotmail.com>
2023-10-10 20:51:22 -0600

    adding hero section padding, header paddin, adding two layers backgrounds

--- src/organisms/Header.tsx
+++ src/organisms/Header.tsx
@@ -2,12 +2,16 @@ const Header = () => {
     return (
         <header className="header">
             <div className="header__container | container">
-                <div className="logo">
-                    <img src="/assets/images/logo-light.svg" alt="workit logo" className="logo__image" />
+                <div className="header__logo">
+                    <img
+                        src="/assets/images/logo-light.svg"
+                        alt="workit logo"
+                        className="header__logo-image"
+                    />
                 </div>
             </div>
         </header>
-    )
+    );
 };
 
 export default Header;
--- src/organisms/HeroSection.tsx
+++ src/organisms/HeroSection.tsx
@@ -1,9 +1,10 @@
-const HeroSection = () => { 
+const HeroSection = () => {
     return (
-        <section className="hero-section">
+        <section className="hero-section | top-background-layer">
+            <div className="bottom-background-layer"></div>
             <div className="container"></div>
         </section>
     );
-}
+};
 
 export default HeroSection;
