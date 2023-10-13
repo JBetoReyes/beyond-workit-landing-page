402041c04bfcffd409d5e0c1a02c23670544bbcc
Beto <josereyes55@hotmail.com>
2023-10-12 21:19:42 -0600

    adding primary button to hero section

--- src/organisms/HeroSection.tsx
+++ src/organisms/HeroSection.tsx
@@ -1,12 +1,16 @@
+import PrimaryButton from "../atoms/PrimaryButton";
+
 const HeroSection = () => {
     return (
         <section className="hero-section | top-background-layer">
             {/* <div className="bottom-background-layer"></div> */}
-            <div className="container">
+            <div className="hero-section__container | container | flow">
                 <h1 className="heading-1">
-                    Data <span className="text-underline">tailored</span> to
+                    Data <span className="text-underline">tailored</span> to{" "}
+                    <br />
                     your needs
                 </h1>
+                <PrimaryButton>Learn More</PrimaryButton>
             </div>
         </section>
     );
