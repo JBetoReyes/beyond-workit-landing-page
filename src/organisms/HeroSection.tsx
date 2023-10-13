041ee3fae4d2d87b65054b3b3d9b3782a6d96f0c
Beto <josereyes55@hotmail.com>
2023-10-12 20:45:54 -0600

    adding headings, fixes hero padding

--- src/organisms/HeroSection.tsx
+++ src/organisms/HeroSection.tsx
@@ -1,8 +1,13 @@
 const HeroSection = () => {
     return (
         <section className="hero-section | top-background-layer">
-            <div className="bottom-background-layer"></div>
-            <div className="container"></div>
+            {/* <div className="bottom-background-layer"></div> */}
+            <div className="container">
+                <h1 className="heading-1">
+                    Data <span className="text-underline">tailored</span> to
+                    your needs
+                </h1>
+            </div>
         </section>
     );
 };
