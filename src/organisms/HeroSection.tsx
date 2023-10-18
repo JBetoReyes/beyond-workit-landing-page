e1d3c1a65547e967cb29042e6c235f16eb9102d2
Beto <josereyes55@hotmail.com>
2023-10-17 20:24:47 -0600

    adding hero section image

--- src/organisms/HeroSection.tsx
+++ src/organisms/HeroSection.tsx
@@ -2,16 +2,21 @@ import PrimaryButton from "../atoms/PrimaryButton";
 
 const HeroSection = () => {
     return (
-        <section className="hero-section | top-background-layer">
+        <section className="hero-section">
             {/* <div className="bottom-background-layer"></div> */}
-            <div className="hero-section__container | container | flow">
-                <h1 className="heading-1">
-                    Data <span className="text-underline">tailored</span> to{" "}
-                    <br />
-                    your needs
-                </h1>
-                <PrimaryButton>Learn More</PrimaryButton>
+            <div className="hero-section__background">
+                <div className="container">
+                    <div className="hero-section__content | flow">
+                        <h1 className="heading-1">
+                            Data <span className="text-underline">tailored</span> to{" "}
+                            <br />
+                            your needs
+                        </h1>
+                        <PrimaryButton>Learn More</PrimaryButton>
+                    </div>
+                </div>
             </div>
+            <img className="hero-section__image" src="/assets/images/image-hero.webp" alt="Iphone 11" />
         </section>
     );
 };
